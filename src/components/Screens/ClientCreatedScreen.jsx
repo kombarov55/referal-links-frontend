5aad568ed935a5adbbdd65c27db55e12b85ae63b
nikolay <kombarov55@gmail.com>
2022-09-04 16:30:57 +0300

    формы

--- src/components/Screens/ClientCreatedScreen.jsx
+++ src/components/Screens/ClientCreatedScreen.jsx
@@ -3,37 +3,43 @@ import PageContent from "../UI/Layout/PageContent";
 import Label from "../UI/UIComponents/Label";
 import {useSearchParams} from "react-router-dom";
 import {BsInstagram, BsTelegram, BsWhatsapp} from "react-icons/bs";
+import SocialButton from "../UI/UIComponents/SocialButton";
 
 export default ({}) => {
     const [params] = useSearchParams();
 
     return <>
-        <PageContent alignItems={"flex-start"}>
-            <Label size={"big"} text={"Спасибо за регистрацию!"}/>
+        <PageContent alignItems={"flex-start"} justifyContent={"flex-start"}>
+            <img src={"logo.png"} style={{width: "100%", marginBottom: "15vh"}}/>
+            <Label size={"big"} text={"Спасибо за регистрацию!"} textAlign={"center"}/>
             <Label text={`Ваш номер телефона (${params.get("phone")}) будет являться номером пользователя.`}/>
-            <Label
-                text={`Теперь вы можете оформить заказ или задать вопрос нашему менеджеру в удобном для вас приложении: (для перехода нажмите на кнопку)`}/>
+            <br/>
+            <Label text={`Теперь вы можете оформить заказ или задать вопрос нашему менеджеру в удобном для вас приложении:`}/>
+            <Label text={`(для перехода нажмите на кнопку)`}/>
             <div style={{
                 display: "flex",
-                flexDirection: "row",
-                alignItems: "center",
-                justifyContent: "justifyContent",
-
+                flexDirection: "column",
+                alignItems: "stretch",
                 width: "100%",
                 gap: "3vw"
             }}>
-                <a href={"https://www.instagram.com/vikupbrendov.rf/"}
-                   style={{color: "black", textDecoration: "none"}}>
-                    <BsInstagram style={{"height": "3vmax", width: "3vmax"}}/>
-                </a>
-                <a href={"https://api.whatsapp.com/send/?phone=79277809082"}
-                   style={{color: "black", textDecoration: "none"}}>
-                    <BsWhatsapp style={{"height": "3vmax", width: "3vmax"}}/>
-                </a>
-                <a href={"https://t.me/vykupbrandrf"}
-                   style={{color: "black", textDecoration: "none"}}>
-                    <BsTelegram style={{"height": "3vmax", width: "3vmax"}}/>
-                </a>
+                <SocialButton iconComponent={<BsInstagram style={{"height": "3vmax", width: "3vmax"}}/>}
+                              url={"https://www.instagram.com/vikupbrendov.rf/"}
+                              text={"Instagram"}
+                              background={"pink"}
+                />
+                <SocialButton iconComponent={<BsWhatsapp style={{"height": "3vmax", width: "3vmax"}}/>}
+                              url={"https://api.whatsapp.com/send/?phone=79277809082/"}
+                              text={"WhatsApp"}
+                              background={"green"}
+                              color={"white"}
+                />
+                <SocialButton iconComponent={<BsTelegram style={{"height": "3vmax", width: "3vmax"}}/>}
+                              url={"https://t.me/vykupbrandrf"}
+                              text={"Telegram"}
+                              background={"#5494e3"}
+                              color={"white"}
+                />
             </div>
         </PageContent>
     </>
